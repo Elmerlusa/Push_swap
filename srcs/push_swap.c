b851bdd198ac7513c2d54ab1644a0532b1f7f072
javmarti <martingallegojavier@outlook.com>
2022-11-19 20:57:39 +0100

    :)

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -12,8 +12,10 @@
 
 #include "push_swap.h"
 
-void	print_stacks(int *stack_a, int *stack_b, int size_a, int size_b);
+// void	print_stacks(int *stack_a, int *stack_b, int size_a, int size_b);
 static void	sort_stack(int **stack_a, int **stack_b, int size);
+static void	sort_algorithm(int **stack_a, int **stack_b, \
+						int *size_a, int *size_b);
 
 int	main(int argc, char *argv[])
 {
@@ -25,7 +27,7 @@ int	main(int argc, char *argv[])
 	stack_a = get_input(argc, argv);
 	stack_b = (int *)ft_calloc(argc - 1, sizeof(int));
 	if (stack_a == NULL || stack_b == NULL)
-		ft_printf("ERROR\n");
+		ft_putstr_fd("Error\n", 2);
 	else
 		sort_stack(&stack_a, &stack_b, argc - 1);
 	free(stack_a);
@@ -36,41 +38,47 @@ int	main(int argc, char *argv[])
 static void	sort_stack(int **stack_a, int **stack_b, int size_a)
 {
 	int	size_b;
-	int	index;
 
 	size_b = 0;
 	while (is_sorted(*stack_a, size_a) == 0)
-	{
-		index = get_min_index(*stack_a, size_a);
-		if (index == size_a - 1)
-		{
-			ft_printf("pb\n");
-			stack_push(stack_b, &size_b, stack_a, &size_a);
-		}
-		else if (index == size_a - 2 &&
-				get_second_min_index(*stack_a, size_a) == size_a - 1)
-		{
-			ft_printf("sa\n");
-			stack_swap(stack_a, size_a);
-		}
-		else if (index >= size_a / 2)
-		{
-			ft_printf("ra\n");
-			stack_rotate(stack_a, size_a);
-		}
-		else
-		{
-			ft_printf("rra\n");
-			stack_reverse_rotate(stack_a, size_a);
-		}
-	}
+		sort_algorithm(stack_a, stack_b, &size_a, &size_b);
 	while (size_b != 0)
 	{
 		ft_printf("pa\n");
 		stack_push(stack_a, &size_a, stack_b, &size_b);
 	}
 	// print_stacks(*stack_a, *stack_b, size_a, size_b);
-	return;
+	return ;
+}
+
+static void	sort_algorithm(int **stack_a, int **stack_b, \
+						int *size_a, int *size_b)
+{
+	int	index;
+
+	index = get_min_index(*stack_a, *size_a);
+	if (index == *size_a - 1)
+	{
+		ft_printf("pb\n");
+		stack_push(stack_b, size_b, stack_a, size_a);
+	}
+	else if (index == *size_a - 2
+		&& get_second_min_index(*stack_a, *size_a) == *size_a - 1)
+	{
+		ft_printf("sa\n");
+		stack_swap(stack_a, *size_a);
+	}
+	else if (index >= *size_a / 2)
+	{
+		ft_printf("ra\n");
+		stack_rotate(stack_a, *size_a);
+	}
+	else
+	{
+		ft_printf("rra\n");
+		stack_reverse_rotate(stack_a, *size_a);
+	}
+	return ;
 }
 
 // void	print_stacks(int *stack_a, int *stack_b, int size_a, int size_b)
